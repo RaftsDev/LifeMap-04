03f3f6a975b544bf4eab632ad91f114f1e18b68b
Ralph <raftsdev@gmail.com>
2022-05-05 10:40:40 -0400

    Chore: Tidy code

--- src/com/lifemap/app/UpdateTask.java
+++ src/com/lifemap/app/UpdateTask.java
@@ -15,8 +15,6 @@ public class UpdateTask {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
 
 		// create session factory
 		SessionFactory factory = new Configuration()
